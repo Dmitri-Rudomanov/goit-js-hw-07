736b237d07740a94a118ca7eff009c31880a2584
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2021-12-02 11:51:47 +0200

    first-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery")
 
-console.log(galleryItems);
+const markup=galleryItems
+.map(item=>
+    `<div class="gallery__item">
+    <a class="gallery__link" href=${item.original}>
+      <img
+        class="gallery__image"
+        src=${item.preview}
+        data-source=${item.original}
+        alt=${item.description}
+      />
+    </a></div>`).join("")
+console.log(markup)
+
+function createImg(markup) {
+gallery.insertAdjacentHTML("beforeend",markup)
+}
+createImg(markup)
+
+
+gallery.addEventListener("click", pictureOnClick)
+
+
+function pictureOnClick(evt){
+evt.preventDefault()
+if(evt.target.nodeName!=="IMG"){
+    return
+}
+    const instance = basicLightbox.create(`
+    <img src=${evt.target.dataset.source} width="800" height="600">
+    `)
+    instance.show()
+}
+
