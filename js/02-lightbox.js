458c9477a68a8bb351bc80ee0d3556e059abe090
Dmitri-Rudomanov <rudomanovdmitrij@gmail.com>
2021-12-03 16:53:23 +0200

    gallery-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector(".gallery")
+
 const markup=galleryItems
 .map(item=>
     `<a class="gallery__item" href=${item.original}>
@@ -15,27 +16,6 @@ gallery.insertAdjacentHTML("beforeend",markup)
 createImg(markup)
 
 
-
-function pictureOnClick(evt){
-evt.preventDefault()
-if(evt.target.nodeName!=="IMG"){
-    return
-}
-    let galleryBox = new SimpleLightbox('.gallery');
-    console.log("here")
-    galleryBox.on("show.simplelightbox", () => {
-        galleryBox.open(evt.target)
-    })
-  document.addEventListener("keydown", (event) => {
-    if (event.code === "Escape") {
-   galleryBox.close()
-  }
-  })
-
-}
-
-
-gallery.addEventListener("click", pictureOnClick)
-
+let lightbox = new SimpleLightbox('.gallery a', {captionDelay:250,captionsData:`alt`});
 
 console.log(galleryItems);
